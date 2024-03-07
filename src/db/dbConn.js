7122241f380b0da8b00b60c1538e7874e30fd8dc
pobitradolai <pobitra0000@gmail.com>
2024-03-07 13:25:34 +0530

    host name change

--- src/db/dbConn.js
+++ src/db/dbConn.js
@@ -1,14 +1,14 @@
 var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'sql6687853'
-    // host     : 'sql6.freemysqlhosting.net',
-    // user     : 'sql6687853',
-    // password : 'GFhwzE2hVa',
+    // host     : 'localhost',
+    // user     : 'root',
+    // password : '',
     // database : 'sql6687853'
+    host     : 'sql6.freemysqlhosting.net',
+    user     : 'sql6687853',
+    password : 'GFhwzE2hVa',
+    database : 'sql6687853'
   });
 
 connection.connect(function(err) {
